7098fc8439fe93b62833bacaeecc3eeededc4ade
VICTOR-81 <v161181@gmail.com>
2020-09-01 15:45:57 +0700

    верстка

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(function(){
-$('.s3__slider').slick({
+$('.s3__slider,.s5__slider').slick({
   prevArrow: '<button type="button" class="slick-btn slick-prev">​<img src="images/arrow-prev.svg" alt=""></button>',
   nextArrow: '<button type="button" class="slick-btn slick-next">​<img src="images/arrow-next.svg" alt=""></button>',
   autoplay: true,
